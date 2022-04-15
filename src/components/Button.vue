afdaa1a62ef4ff313d2db4ed9a66a8e41da7bf27
remysonnevill <74176338+remysonneville@users.noreply.github.com>
2022-04-15 18:54:01 +0200

    todolist

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button v-on:click="onClick()" :style="{background: color}" class="button">{{text}}</button>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Button',
+  props: {
+    text: String,
+    color: String
+  },
+  methods: {
+    onClick () {
+      this.$emit('toggle-add-task')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <Button @toggle-add-task="$emit('toggle-add-task')" text="Ajouter" color="black" />
+  </header>
+
+</template>
+
+<script>
+import Button from '@/components/Button'
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Header',
+  props: {
+    title: String
+  },
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
